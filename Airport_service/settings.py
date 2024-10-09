a40e2c08481597140d33f4e372fcf10d76eefe35
Ostap <ostapbojko04@gmail.com>
2024-10-09 21:14:55 +0300

    add rest_framework to INSTALLED_APP

--- Airport_service/settings.py
+++ Airport_service/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Airport',
+    'rest_framework',
     'rest_framework_simplejwt',
+    'Airport',
 ]
 
 MIDDLEWARE = [
