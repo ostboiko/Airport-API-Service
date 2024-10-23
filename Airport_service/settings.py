cd47644f070477b9385d8490336ae5c4c77f57ec
Ostap <ostapbojko04@gmail.com>
2024-10-23 20:31:56 +0300

    update DATABASE in settings.py

--- Airport_service/settings.py
+++ Airport_service/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Airport_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_db',
+        'USER': 'postgres_user',
+        'PASSWORD': 'postgres_password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
