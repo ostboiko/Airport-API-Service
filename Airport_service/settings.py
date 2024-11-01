46d32e84d23f916fcdda802030ae957732b65701
Ostap <ostapbojko04@gmail.com>
2024-11-01 21:21:40 +0200

    add SECRET_KEY to .env and edit docker-compose.yaml

--- Airport_service/settings.py
+++ Airport_service/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
+secret_key = os.getenv('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
